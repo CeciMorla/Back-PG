8ea3bf55ccde5ae4c40a5e2302516bf159cb40c0
Cecilia Morlacchi <90002130+CeciMorla@users.noreply.github.com>
2022-02-20 18:48:14 -0300

    Update newsletter.js

--- src/routes/newsletter.js
+++ src/routes/newsletter.js
@@ -52,12 +52,14 @@ router.post("/", async (req, res, next) => {
          `,
       };
 
-      transporter.sendMail(mailOption, (error, info) => {
-        if (error) {
-          res.status(500).send(error.message);
-        } else {
-          res.status(200).json(req.body); //shows <---> province
-        }
+      await new Promise((resolve, reject) => {
+        transporter.sendMail(mailOption, (error, info) => {
+          if (error) {
+            res.status(500).send(error.message);
+          } else {
+            res.status(200).json(req.body);
+          }
+        });
       });
     });
   });
